df592af094e6013bbd54a0d377a50a706a720343
Alex Melnikov <alexmlnkv@gmail.com>
2021-05-30 21:18:01 +0300

    project init & написать глобальные стили, создать Navbar.js

---
+++ src/Components/NavBar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../img/logo.svg';
+import loginImg from '../img/sign.svg';
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: #299B01;
+  color: white;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const H1 = styled.h1`
+  font-size: 24px;
+  margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+  width: 50px;
+`;
+
+const LoginImg = styled.img`
+  width: 30px;
+  margin-bottom: 5px;
+`;
+
+const LoginBtn = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: inherit;
+  border: none;
+  color: #FFFFFF;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+
+
+
+export const NavBar = () => (
+  <NavBarStyled>
+    <Logo>
+      <ImgLogo src={logoImg} alt="logo"/>
+      <H1>MrDonald's</H1>
+    </Logo>
+    <LoginBtn><LoginImg src={loginImg} />войти</LoginBtn>
+    
+  </NavBarStyled>
+);
